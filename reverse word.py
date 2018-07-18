1b57c3924dd57cade0321980e10c4dbcb0678297
rakheshprasaath <37951918+rakheshprasaath@users.noreply.github.com>
2018-07-18 21:40:39 +0530

    Add files via upload

---
+++ reverse word.py
@@ -0,0 +1,19 @@
+def reverse_word(strr):
+
+    strr=strr.split(" ")
+
+    strr=strr[-1::-1]
+
+    strr= " ".join(strr)
+
+    return(strr)
+
+
+
+
+
+str=input()
+
+
+print("the reverse word is")
+print(reverse_word(str))
